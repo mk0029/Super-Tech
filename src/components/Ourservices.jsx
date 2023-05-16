be1108337e2d446a347ecd979176dddc397b656e
mk0029 <mkhicher404@gmail.com>
2023-05-16 11:18:21 +0530

    Solve//Err

---
+++ src/components/Ourservices.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Ourservices() {
+  return (
+    <>
+      <section>
+        {" "}
+        <div className=" "></div>
+      </section>
+    </>
+  );
+}
+
+export default Ourservices;
