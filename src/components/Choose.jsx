19e8423e8e8c06c92b9b481e7eaea5b8d2e04cd1
Mj-Saini <manojsaini97285@gmail.com>
2023-05-18 16:32:28 +0530

    update-honey

--- src/components/Choose.jsx
+++ src/components/Choose.jsx
@@ -14,42 +14,42 @@ function Choose() {
             <div className="grid_container mt-5">
               <div className="item_1 grid_boxes bg-danger">
                 <div style={{ background: "#f7fdf6" }} className="grid_box">
-                  <h1
+                  <h2
                     style={{ color: "#5FCE43" }}
                     className=" ff_jakarta fw-semibold fs_2x8l text-center"
                   >
                     Supportive
-                  </h1>
+                  </h2>
                 </div>
               </div>
               <div className="item_2 grid_boxes">
                 <div className="grid_box">
-                  <h1
+                  <h2
                     style={{ color: "#CF1818" }}
                     className=" ff_jakarta fw-semibold fs_2x8l text-center"
                   >
                     Empathetic
-                  </h1>
+                  </h2>
                 </div>
               </div>
               <div className="item_3 grid_boxes">
                 <div className="grid_box">
-                  <h1
+                  <h2
                     style={{ color: "#E5BD26" }}
                     className=" ff_jakarta fw-semibold fs_2x8l text-center"
                   >
                     Engaging
-                  </h1>
+                  </h2>
                 </div>
               </div>
               <div className="item_4 grid_boxes">
                 <div className="grid_box">
-                  <h1
+                  <h2
                     style={{ color: "#EC6A15" }}
                     className=" ff_jakarta fw-semibold fs_2x8l text-center"
                   >
                     Progressive
-                  </h1>
+                  </h2>
                 </div>
               </div>
               <div className="item_5 grid_boxes">
@@ -59,52 +59,52 @@ function Choose() {
               </div>
               <div className="item_6 grid_boxes">
                 <div className="grid_box">
-                  <h1
+                  <h2
                     style={{ color: "#186CCF" }}
                     className=" ff_jakarta fw-semibold fs_2x8l text-center"
                   >
                     Agile
-                  </h1>
+                  </h2>
                 </div>
               </div>
               <div className="item_7 grid_boxes">
                 <div className="grid_box">
-                  <h1
+                  <h2
                     style={{ color: "#186CCF" }}
                     className=" ff_jakarta fw-semibold fs_2x8l text-center"
                   >
                     Analytical
-                  </h1>
+                  </h2>
                 </div>
               </div>
               <div className="item_8 grid_boxes">
                 <div className="grid_box">
-                  <h1
+                  <h2
                     style={{ color: "#8E39AB" }}
                     className=" ff_jakarta fw-semibold fs_2x8l text-center"
                   >
                     Comprehensive
-                  </h1>
+                  </h2>
                 </div>
               </div>
               <div className="item_9 grid_boxes">
                 <div className="grid_box">
-                  <h1
+                  <h2
                     style={{ color: "#0EB196" }}
                     className=" ff_jakarta fw-semibold fs_2x8l text-center"
                   >
                     Receptive
-                  </h1>
+                  </h2>
                 </div>
               </div>
               <div className="item_10 grid_boxes">
                 <div className="grid_box">
-                  <h1
+                  <h2
                     style={{ color: "#00A3FF" }}
                     className=" ff_jakarta fw-semibold fs_2x8l text-center"
                   >
                     Direct
-                  </h1>
+                  </h2>
                 </div>
               </div>
             </div>
