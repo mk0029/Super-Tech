6eaf083da8f230386825a42905a5bc3e4d5b88d1
mk0029 <mkhicher404@gmail.com>
2023-05-20 14:00:42 +0530

    Add//Loader on New Section

--- src/Pagejs/Policy.js
+++ src/Pagejs/Policy.js
@@ -3,11 +3,14 @@ import ServicesHero from "../components/ServicesHero";
 import PrivacyPolicyBg from "../assets/img/webp/Privecypolicy.webp";
 import PrivaryPolicy from "../components/PrivaryPolicy";
 import Footer from "../components/Footer";
+import Loader from "../components/Loader";
 
 function Policy() {
   const Heading = "Privacy Policy ";
   return (
     <>
+      {" "}
+      <Loader timeOut="1500" />
       <ServicesHero heroHeading={Heading} heroBG={PrivacyPolicyBg} />
       <PrivaryPolicy />
       <Footer />
--- src/Pagejs/TremCondition.js
+++ src/Pagejs/TremCondition.js
@@ -3,12 +3,13 @@ import Condition from "../components/Condition";
 import ServicesHero from "../components/ServicesHero";
 import PrivacyPolicyBg from "../assets/img/webp/Privecypolicy.webp";
 import Footer from "../components/Footer";
+import Loader from "../components/Loader";
 
 function TremCondition() {
   const Heading = "Terms And Conditions ";
   return (
     <>
-      {" "}
+      <Loader timeOut="1500" />
       <ServicesHero heroHeading={Heading} heroBG={PrivacyPolicyBg} />
       <Condition />
       <Footer />
