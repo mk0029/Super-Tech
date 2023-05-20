9e55b7c4dcfab2d9401feeb8e0ed6bfad4264890
Mj-Saini <manojsaini97285@gmail.com>
2023-05-20 14:13:51 +0530

    wrap-in-div

--- src/components/Condition.jsx
+++ src/components/Condition.jsx
@@ -20,125 +20,148 @@ function Condition() {
                   be bound by these Terms. If you do not agree with any part of
                   these Terms, you may not use our services.
                 </p>
-                <h2 className="ff_jakarta fw-semibold fs_2x8l text_black mb-0 mt-4 pt-2">
-                  General Terms
-                </h2>
-                <p className="ff_jakarta fw-normal fs_sm text_black opacity08 mb-0 pt-2 mt-1">
-                  Supertech IT Consulting provides IT consulting and related
-                  services to clients. The specific services and terms of
-                  engagement will be outlined in a separate agreement between
-                  the Company and the Client.
-                </p>
-                <h2 className="ff_jakarta fw-semibold fs_2x8l text_black mb-0 mt-4 pt-2">
-                  Client Obligations
-                </h2>
-                <p className="ff_jakarta fw-normal fs_sm text_black opacity08 mb-0 pt-2 mt-1">
-                  <span className="d-block">
-                    The Client agrees to cooperate with the Company, providing
-                    accurate and complete information necessary for the
-                    provision of services.
-                  </span>{" "}
-                  <span className="d-block">
-                    The Client is responsible for the security and
-                    confidentiality of any credentials or access provided to the
-                    Company to perform the services.
-                  </span>{" "}
-                  The Client acknowledges that the Company's recommendations and
-                  advice are based on the information provided by the Client and
-                  cannot guarantee specific results or outcomes.
-                </p>
-                <h2 className="ff_jakarta fw-semibold fs_2x8l text_black mb-0 mt-4 pt-2">
-                  Fees and Payment
-                </h2>
-                <p className="ff_jakarta fw-normal fs_sm text_black opacity08 mb-0 pt-2 mt-1">
-                  <span className="d-block">
-                    The fees for the services provided by the Company will be
-                    outlined in the separate agreement between the Company and
-                    the Client.
-                  </span>{" "}
-                  Unless otherwise stated, all fees are due and payable within
-                  [number] days from the date of invoice. Late payments may be
-                  subject to late fees or suspension of services.
-                </p>
-                <h2 className="ff_jakarta fw-semibold fs_2x8l text_black mb-0 mt-4 pt-2">
-                  Intellectual Property
-                </h2>
-                <p className="ff_jakarta fw-normal fs_sm text_black opacity08 mb-0 pt-2 mt-1">
-                  <span className="d-block">
-                    The Company retains all intellectual property rights,
-                    including but not limited to copyrights, trademarks, and
-                    trade secrets, related to the services provided.{" "}
-                  </span>{" "}
-                  The Client agrees not to reproduce, distribute, modify, or
-                  create derivative works based on the Company's intellectual
-                  property without prior written consent.
-                </p>
-                <h2 className="ff_jakarta fw-semibold fs_2x8l text_black mb-0 mt-4 pt-2">
-                  Confidentiality
-                </h2>
-                <p className="ff_jakarta fw-normal fs_sm text_black opacity08 mb-0 pt-2 mt-1">
-                  The Company and the Client acknowledge that during the
-                  provision of services, confidential information may be shared.
-                  Both parties agree to keep such information confidential and
-                  not disclose it to third parties, except as required by law.
-                </p>
-                <h2 className="ff_jakarta fw-semibold fs_2x8l text_black mb-0 mt-4 pt-2">
-                  Limitation of Liability
-                </h2>
-                <p className="ff_jakarta fw-normal fs_sm text_black opacity08 mb-0 pt-2 mt-1">
-                  <span className="d-block">
-                    The Company's liability, whether in contract, tort, or
-                    otherwise, arising out of or in connection with the services
-                    provided, shall be limited to the amount paid by the Client
-                    for the specific services.
-                  </span>{" "}
-                  The Company shall not be liable for any indirect, incidental,
-                  consequential, or punitive damages arising out of or relating
-                  to the services or the use of the services.
-                </p>
-                <h2 className="ff_jakarta fw-semibold fs_2x8l text_black mb-0 mt-4 pt-2">
-                  Indemnification
-                </h2>
-                <p className="ff_jakarta fw-normal fs_sm text_black opacity08 mb-0 pt-2 mt-1">
-                  The Client agrees to indemnify and hold the Company, its
-                  officers, directors, employees, and agents harmless from any
-                  claims, damages, liabilities, or expenses arising out of or
-                  related to the Client's use of the services or breach of these
-                  Terms.
-                </p>
-                <h2 className="ff_jakarta fw-semibold fs_2x8l text_black mb-0 mt-4 pt-2">
-                  Termination
-                </h2>
-                <p className="ff_jakarta fw-normal fs_sm text_black opacity08 mb-0 pt-2 mt-1">
-                  Either party may terminate the services provided under these
-                  Terms by providing written notice to the other party. Any
-                  outstanding fees or obligations shall remain payable upon
-                  termination.
-                </p>
-                <h2 className="ff_jakarta fw-semibold fs_2x8l text_black mb-0 mt-4 pt-2">
-                  Governing Law and Jurisdiction
-                </h2>
-                <p className="ff_jakarta fw-normal fs_sm text_black opacity08 mb-0 pt-2 mt-1">
-                  These Terms shall be governed by and construed in accordance
-                  with the laws . Any disputes arising out of or relating to
-                  these Terms shall be subject to the exclusive jurisdiction of
-                  the courts in .
-                </p>
-                <h2 className="ff_jakarta fw-semibold fs_2x8l text_black mb-0 mt-4 pt-2">
-                  Amendments
-                </h2>
-                <p className="ff_jakarta fw-normal fs_sm text_black opacity08 mb-0 pt-2 mt-1">
-                  The Company reserves the right to modify or update these Terms
-                  at any time without prior notice. Any changes will be
-                  effective upon posting the revised Terms on the Company's
-                  website or providing notice to the Client.
-                </p>
-                <h2 className="ff_jakarta fw-semibold fs_2x8l text_black mb-0 mt-4 pt-2">
-                  Contact Information
-                </h2>
-                <p className="ff_jakarta fw-normal fs_sm text_black opacity08 mb-0 pt-2 mt-1">
-                  If you have any questions or concerns about these Terms
-                </p>
+                <div>
+                  <h2 className="ff_jakarta fw-semibold fs_2x8l text_black mb-0 mt-4 pt-2">
+                    General Terms
+                  </h2>
+                  <p className="ff_jakarta fw-normal fs_sm text_black opacity08 mb-0 pt-2 mt-1">
+                    Supertech IT Consulting provides IT consulting and related
+                    services to clients. The specific services and terms of
+                    engagement will be outlined in a separate agreement between
+                    the Company and the Client.
+                  </p>
+                </div>
+                <div>
+                  <h2 className="ff_jakarta fw-semibold fs_2x8l text_black mb-0 mt-4 pt-2">
+                    Client Obligations
+                  </h2>
+                  <p className="ff_jakarta fw-normal fs_sm text_black opacity08 mb-0 pt-2 mt-1">
+                    <span className="d-block">
+                      The Client agrees to cooperate with the Company, providing
+                      accurate and complete information necessary for the
+                      provision of services.
+                    </span>{" "}
+                    <span className="d-block">
+                      The Client is responsible for the security and
+                      confidentiality of any credentials or access provided to
+                      the Company to perform the services.
+                    </span>{" "}
+                    The Client acknowledges that the Company's recommendations
+                    and advice are based on the information provided by the
+                    Client and cannot guarantee specific results or outcomes.
+                  </p>
+                </div>
+                <div>
+                  <h2 className="ff_jakarta fw-semibold fs_2x8l text_black mb-0 mt-4 pt-2">
+                    Fees and Payment
+                  </h2>
+                  <p className="ff_jakarta fw-normal fs_sm text_black opacity08 mb-0 pt-2 mt-1">
+                    <span className="d-block">
+                      The fees for the services provided by the Company will be
+                      outlined in the separate agreement between the Company and
+                      the Client.
+                    </span>{" "}
+                    Unless otherwise stated, all fees are due and payable within
+                    [number] days from the date of invoice. Late payments may be
+                    subject to late fees or suspension of services.
+                  </p>
+                </div>
+                <div>
+                  <h2 className="ff_jakarta fw-semibold fs_2x8l text_black mb-0 mt-4 pt-2">
+                    Intellectual Property
+                  </h2>
+                  <p className="ff_jakarta fw-normal fs_sm text_black opacity08 mb-0 pt-2 mt-1">
+                    <span className="d-block">
+                      The Company retains all intellectual property rights,
+                      including but not limited to copyrights, trademarks, and
+                      trade secrets, related to the services provided.{" "}
+                    </span>{" "}
+                    The Client agrees not to reproduce, distribute, modify, or
+                    create derivative works based on the Company's intellectual
+                    property without prior written consent.
+                  </p>
+                </div>
+                <div>
+                  <h2 className="ff_jakarta fw-semibold fs_2x8l text_black mb-0 mt-4 pt-2">
+                    Confidentiality
+                  </h2>
+                  <p className="ff_jakarta fw-normal fs_sm text_black opacity08 mb-0 pt-2 mt-1">
+                    The Company and the Client acknowledge that during the
+                    provision of services, confidential information may be
+                    shared. Both parties agree to keep such information
+                    confidential and not disclose it to third parties, except as
+                    required by law.
+                  </p>
+                </div>
+                <div>
+                  <h2 className="ff_jakarta fw-semibold fs_2x8l text_black mb-0 mt-4 pt-2">
+                    Limitation of Liability
+                  </h2>
+                  <p className="ff_jakarta fw-normal fs_sm text_black opacity08 mb-0 pt-2 mt-1">
+                    <span className="d-block">
+                      The Company's liability, whether in contract, tort, or
+                      otherwise, arising out of or in connection with the
+                      services provided, shall be limited to the amount paid by
+                      the Client for the specific services.
+                    </span>{" "}
+                    The Company shall not be liable for any indirect,
+                    incidental, consequential, or punitive damages arising out
+                    of or relating to the services or the use of the services.
+                  </p>
+                </div>
+                <div>
+                  <h2 className="ff_jakarta fw-semibold fs_2x8l text_black mb-0 mt-4 pt-2">
+                    Indemnification
+                  </h2>
+                  <p className="ff_jakarta fw-normal fs_sm text_black opacity08 mb-0 pt-2 mt-1">
+                    The Client agrees to indemnify and hold the Company, its
+                    officers, directors, employees, and agents harmless from any
+                    claims, damages, liabilities, or expenses arising out of or
+                    related to the Client's use of the services or breach of
+                    these Terms.
+                  </p>
+                </div>
+                <div>
+                  <h2 className="ff_jakarta fw-semibold fs_2x8l text_black mb-0 mt-4 pt-2">
+                    Termination
+                  </h2>
+                  <p className="ff_jakarta fw-normal fs_sm text_black opacity08 mb-0 pt-2 mt-1">
+                    Either party may terminate the services provided under these
+                    Terms by providing written notice to the other party. Any
+                    outstanding fees or obligations shall remain payable upon
+                    termination.
+                  </p>
+                </div>
+                <div>
+                  <h2 className="ff_jakarta fw-semibold fs_2x8l text_black mb-0 mt-4 pt-2">
+                    Governing Law and Jurisdiction
+                  </h2>
+                  <p className="ff_jakarta fw-normal fs_sm text_black opacity08 mb-0 pt-2 mt-1">
+                    These Terms shall be governed by and construed in accordance
+                    with the laws . Any disputes arising out of or relating to
+                    these Terms shall be subject to the exclusive jurisdiction
+                    of the courts in .
+                  </p>
+                </div>
+                <div>
+                  <h2 className="ff_jakarta fw-semibold fs_2x8l text_black mb-0 mt-4 pt-2">
+                    Amendments
+                  </h2>
+                  <p className="ff_jakarta fw-normal fs_sm text_black opacity08 mb-0 pt-2 mt-1">
+                    The Company reserves the right to modify or update these
+                    Terms at any time without prior notice. Any changes will be
+                    effective upon posting the revised Terms on the Company's
+                    website or providing notice to the Client.
+                  </p>
+                </div>
+                <div>
+                  <h2 className="ff_jakarta fw-semibold fs_2x8l text_black mb-0 mt-4 pt-2">
+                    Contact Information
+                  </h2>
+                  <p className="ff_jakarta fw-normal fs_sm text_black opacity08 mb-0 pt-2 mt-1">
+                    If you have any questions or concerns about these Terms
+                  </p>
+                </div>
               </div>
             </div>
           </div>
